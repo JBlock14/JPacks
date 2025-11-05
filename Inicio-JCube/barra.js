6ea7b5439522a32a251676584f56e9e82ea6d4a0
JBlock14 <minecraftjafocraft@gmail.com>
2025-11-05 16:44:42 -0500

    Add files via upload

---
+++ Inicio-JCube/barra.js
@@ -0,0 +1,22 @@
+function animarLogoYRedirigir(nombre, url) {
+  const logoMap = {
+    modrinth: "assets/gui/logo/modrinth.png",
+    curseforge: "assets/gui/logo/curseforge.png",
+    planetmc: "assets/gui/logo/planetmc.png",
+    youtube: "assets/gui/logo/youtube.png",
+    discord: "assets/gui/logo/dc.png"
+  };
+
+  const overlay = document.getElementById("logoOverlay");
+  const img = document.getElementById("logoImage");
+
+  img.src = logoMap[nombre];
+  overlay.classList.remove("show");
+  void overlay.offsetWidth;
+  overlay.classList.add("show");
+
+  setTimeout(() => {
+    overlay.classList.remove("show");
+    window.open(url, "_blank");
+  }, 600);
+}
